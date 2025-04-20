52de8f99e34e0067be5ba1ec561b49510e662510
Dmitrii Mazilkin <dim.mazilkin@gmail.com>
2025-04-20 10:10:31 +0200

    Remove unsused imports, add asyncio handlers implementations.

--- src/server.py
+++ src/server.py
@@ -1,4 +1,6 @@
 import uvicorn
+import asyncio
+from time import sleep
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse, HTMLResponse, PlainTextResponse
@@ -14,18 +16,22 @@ templates = Jinja2Templates('templates')
 nn = NeuralNetwork()
 
 @app.get('/', response_class=HTMLResponse)
-def home(request: Request):
-    return templates.TemplateResponse('home.html', {'request': request})
+async def home(request: Request):
+    loop = asyncio.get_event_loop()
+    template = await loop.run_in_executor(None, templates.TemplateResponse, 'home.html', {'request': request})
+    return template
 
 @app.get('/func', response_class=PlainTextResponse)
-def get_func(x1: float, x2: float, x3: float, x4: float):
-    predict = nn.predict([[x1, x2, x3, x4]])[0][0]
-    return PlainTextResponse(str(predict))
+async def get_func(x1: float, x2: float, x3: float, x4: float):
+    loop = asyncio.get_event_loop()
+    predict = await loop.run_in_executor(None, nn.predict, [[x1, x2, x3, x4]])
+    return PlainTextResponse(str(predict[0][0]))
 
 @app.post('/func')
-def post_func(data: List[List[float]]):
-    predict = nn.predict(data)[0]
-    return JSONResponse(predict)
+async def post_func(data: List[List[float]]):
+    loop = asyncio.get_event_loop()
+    predict = await loop.run_in_executor(None, nn.predict, data)
+    return JSONResponse(predict[0])
 
 def main():
     # Configure and start Uvicorn server
